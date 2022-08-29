50a6e778a27a09a36ffd72a14e61ef6f57c27ecd
t_ateya <ateya.api@gmail.com>
2022-08-28 23:55:45 -0500

    Fixed PostgreSQL compatibility issues

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "ateya123"
 api = Api(app)
 
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
 jwt = JWT(app, authenticate, identity)
 
