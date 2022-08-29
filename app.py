309691df5acc7df0b6d17acd9f42cbd784b3052d
t_ateya <ateya.api@gmail.com>
2022-08-28 20:51:04 -0500

    Fixed not importing db when running the app

--- app.py
+++ app.py
@@ -15,11 +15,6 @@ app.secret_key = "ateya123"
 api = Api(app)
 
 
-@app.before_first_request
-def create_table():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Store, '/store/<string:name>')
